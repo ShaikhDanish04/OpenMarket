28f7d2c860585ac0b3288ba28f044304c5f7c80c
ShaikhDanish04 <32537559+shaikh44@users.noreply.github.com>
2020-03-30 00:54:47 +0530

    Shop List and Product List Structured

--- buyer/view/home.php
+++ buyer/view/home.php
@@ -1,12 +1,120 @@
-<div id="buyer_process" class="carousel slide" data-ride="carousel" data-slide="false" data-interval="false" data-wrap="false">
+<style>
+    .shop-card {
+        display: flex;
+        height: 150px;
+        flex-direction: row;
+    }
 
+    .card-side-img {
+        height: 100%;
+        width: 135px;
+        background: #999997;
+        background: url(img/shop_dummy.jpg);
+        background-size: cover;
+        background-position: bottom;
+        background-repeat: no-repeat;
+    }
+
+    .shop-card .card-body {
+        padding: 0.75rem 1rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    .shop-card .card-title {
+        border-bottom: 1px solid #ccc;
+        padding-bottom: .25rem;
+        font-weight: 500;
+        margin-bottom: .25rem;
+    }
+
+    .product-card {
+        display: flex;
+        min-height: 150px;
+        flex-direction: row;
+        margin-bottom: 1rem;
+    }
+
+    .product-card .card-side-img {
+        height: auto;
+        flex-shrink: 0;
+    }
+
+    .product-card .card-title {
+        border-bottom: 1px solid #ccc;
+        padding-bottom: .25rem;
+        font-weight: 500;
+        margin-bottom: .25rem;
+        font-size: 13px;
+    }
+
+    .product-card .card-text {
+        font-size: 14px;
+    }
+
+    .order-list {
+        position: fixed;
+        border-radius: 5px;
+        bottom: 10px;
+        left: 10px;
+        right: 10px;
+        box-shadow: 0 0 12px #9c9c9c;
+    }
+
+    .order-list .items-in-list {
+        max-height: 70vh;
+        overflow-y: scroll;
+    }
+
+    .product-carousel {
+        margin-bottom: 50px;
+    }
+
+    .carousel-item {
+        min-height: 95vh;
+    }
+</style>
+
+
+<div id="buyer_process" class="carousel slide" data-ride="carousel" data-slide="false" data-interval="false" data-wrap="false">
     <!-- The slideshow -->
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <?php include("home/shop_list.php") ?>
+            <div class="container py-3">
+                <div class="form-group d-flex">
+                    <input type="text" name="search_shop" class="form-control" placeholder="Search">
+                    <button class="btn btn-secondary ml-2" data-toggle="collapse" data-target="#filter_container"><i class="fa fa-filter"></i></button>
+                </div>
+                <div id="filter_container" class="container collapse">
+                    <div class="card mb-3">
+                        <div class="card-body">
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="shop-list"></div>
+
+            </div>
         </div>
         <div class="carousel-item product-carousel">
-            <?php include("home/product_list.php") ?>
+            <div class="container py-3">
+                <input type="hidden" name="shop_id">
+                <input type="hidden" name="buyer_id" value="<?php echo $id ?>">
+
+                <div class="form-group d-flex align-items-center justify-content-between">
+                    <a href="#buyer_process" data-slide="prev" class="btn"><i class="fa fa-chevron-left text-dark"></i></a>
+                    <p class="h6 mb-0">Products</p>
+                    <a class="btn" data-toggle="collapse" data-target="#filter_product"><i class="fa fa-search text-dark"></i></a>
+                </div>
+                <div id="filter_product" class="container collapse">
+                    <input type="text" name="search_shop" class="form-control mb-3" placeholder="Search">
+                </div>
+
+                <div class="product_list_container"></div>
+
+            </div>
         </div>
     </div>
 </div>
@@ -34,77 +142,15 @@
     </div>
 </div>
 
-<script>
-    $("#buyer_process").on('slid.bs.carousel', function() {
-        if ($('.product-carousel').hasClass('active')) {
-            $.ajax({
-                type: "POST",
-                url: "request/manage_order_list.php",
-                data: {
-                    "operation": "get_order_list",
-                    "shop_id": $('[name="shop_id"]').val()
-                },
-                success: function(data) {
-                    $('.items-in-list').html(data);
-                    if (data != '') {
-                        $('.order-list').slideDown();
-                    }
-                }
-            });
-        } else {
-            $('.order-list').slideUp();
-        }
-    });
-    $('[data-target="#check_list"]').click(function() {
-        $.ajax({
-            type: "POST",
-            url: "request/manage_order_list.php",
-            data: {
-                "operation": "get_order_list",
-                "shop_id": $('[name="shop_id"]').val()
-            },
-            success: function(data) {
-                $('.items-in-list').html(data);
-                if (data != '') {
-                    $('.order-list').slideDown();
-                }
-            }
-        })
-    });
-</script>
-<style>
-    .order-list {
-        position: fixed;
-        border-radius: 5px;
-        bottom: 10px;
-        left: 10px;
-        right: 10px;
-        box-shadow: 0 0 12px #9c9c9c;
-    }
-
-    .order-list .items-in-list {
-        max-height: 70vh;
-        overflow-y: scroll;
-    }
-
-    .product-carousel {
-        margin-bottom: 50px;
-    }
-</style>
-
-
-<!-- The Modal -->
 <div class="modal" id="book_product">
     <div class="modal-dialog ">
         <div class="modal-content">
             <form action="" method="post">
-                <!-- Modal Header -->
                 <div class="modal-header">
                     <h4 class="modal-title">Book Product</h4>
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
                 </div>
 
-                <!-- Modal body -->
                 <div class="modal-body">
                     <p class="">Product Name : <b class="product_name_get"></b></p>
                     <input type="hidden" name="operation" value="add_order">
@@ -124,37 +170,6 @@
                         <small class="text-muted">*Required</small>
                     </div>
 
-                    <script>
-                        $(document).ready(function() {
-
-                            $('.minus-val').click(function() {
-                                $(this).next().val(Number($(this).next().val()) - Number(1));
-                                $(this).next().change();
-                            })
-                            $('.plus-val').click(function() {
-                                $(this).prev().val(Number($(this).prev().val()) + Number(1));
-                                $(this).prev().change();
-                            })
-
-                            $('[name="quantity_of_items"],.modal-body').on('change click', function() {
-                                var value = $('[name="quantity_of_items"]').val();
-                                var unit = value.toString().split(".");
-
-                                if ($('[name="sold_by"]').val() == "Kg") {
-                                    $('.estimation').text(unit[0] + ' kilo ' + Number((value * 1000).toString().slice(-3)) + ' gram');
-                                }
-                                if ($('[name="sold_by"]').val() == "Liter") {
-                                    $('.estimation').text(unit[0] + ' liter ' + Number((value * 1000).toString().slice(-3)) + ' ml');
-                                }
-                                if ($('[name="sold_by"]').val() == "Unit") {
-                                    $('.estimation').text(unit[0] + ' Unit ');
-                                    $(this).attr('step', '1');
-                                } else {
-                                    $(this).attr('step', '0.005');
-                                }
-                            })
-                        })
-                    </script>
                     <div class="mt-3 d-flex justify-content-between">
                         <button type="button" class="btn btn-sm btn-danger" data-dismiss="modal">Close</button>
                         <button type="Submit" class="btn btn-sm btn-success">Submit</button>
@@ -165,26 +180,19 @@
         </div>
     </div>
 </div>
-<style>
-    .carousel-item {
-        min-height: 95vh;
-    }
-</style>
-
 <script>
-    $('form').submit(function(e) {
-        e.preventDefault(); // avoid to execute the actual submit of the form.
+    $(document).ready(function() {
+        $('[name="search_shop"]').on("keyup", function() {
+            var value = $(this).val().toLowerCase();
+            $(".shop-list .shop-card").filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+        });
 
-        $.ajax({
-            type: "POST",
-            url: "request/manage_order_list.php",
-            data: $(this).serialize(), // serializes the form's elements.
-            success: function(data) {
-                // location.reload();
-                console.log(data);
-                $('#book_product').modal('hide');
+        $('.shop-list').load('request/shop_list.php');
 
-                
+        $("#buyer_process").on('slid.bs.carousel', function() {
+            if ($('.product-carousel').hasClass('active')) {
                 $.ajax({
                     type: "POST",
                     url: "request/manage_order_list.php",
@@ -193,15 +201,90 @@
                         "shop_id": $('[name="shop_id"]').val()
                     },
                     success: function(data) {
-                        // location.reload();
-                        // console.log(data);
                         $('.items-in-list').html(data);
                         if (data != '') {
                             $('.order-list').slideDown();
                         }
                     }
-                })
+                });
+            } else {
+                $('.order-list').slideUp();
             }
         });
+        $('[data-target="#check_list"]').click(function() {
+            $.ajax({
+                type: "POST",
+                url: "request/manage_order_list.php",
+                data: {
+                    "operation": "get_order_list",
+                    "shop_id": $('[name="shop_id"]').val()
+                },
+                success: function(data) {
+                    $('.items-in-list').html(data);
+                    if (data != '') {
+                        $('.order-list').slideDown();
+                    }
+                }
+            })
+        });
+
+        $('.minus-val').click(function() {
+            $(this).next().val(Number($(this).next().val()) - Number(1));
+            $(this).next().change();
+        })
+        $('.plus-val').click(function() {
+            $(this).prev().val(Number($(this).prev().val()) + Number(1));
+            $(this).prev().change();
+        })
+
+        $('[name="quantity_of_items"],.modal-body').on('change click', function() {
+            var value = $('[name="quantity_of_items"]').val();
+            var unit = value.toString().split(".");
+
+            if ($('[name="sold_by"]').val() == "Kg") {
+                $('.estimation').text(unit[0] + ' kilo ' + Number((value * 1000).toString().slice(-3)) + ' gram');
+            }
+            if ($('[name="sold_by"]').val() == "Liter") {
+                $('.estimation').text(unit[0] + ' liter ' + Number((value * 1000).toString().slice(-3)) + ' ml');
+            }
+            if ($('[name="sold_by"]').val() == "Unit") {
+                $('.estimation').text(unit[0] + ' Unit ');
+                $(this).attr('step', '1');
+            } else {
+                $(this).attr('step', '0.005');
+            }
+        })
+        $('form').submit(function(e) {
+            e.preventDefault(); // avoid to execute the actual submit of the form.
+
+            $.ajax({
+                type: "POST",
+                url: "request/manage_order_list.php",
+                data: $(this).serialize(), // serializes the form's elements.
+                success: function(data) {
+                    // location.reload();
+                    console.log(data);
+                    $('#book_product').modal('hide');
+
+
+                    $.ajax({
+                        type: "POST",
+                        url: "request/manage_order_list.php",
+                        data: {
+                            "operation": "get_order_list",
+                            "shop_id": $('[name="shop_id"]').val()
+                        },
+                        success: function(data) {
+                            // location.reload();
+                            // console.log(data);
+                            $('.items-in-list').html(data);
+                            if (data != '') {
+                                $('.order-list').slideDown();
+                            }
+                        }
+                    })
+                }
+            });
+        })
     })
 </script>
