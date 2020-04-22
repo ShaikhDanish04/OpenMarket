76b3d9b65c317a720b2fdf4a7c105d0c9a67dbff
ShaikhDanish04 <32537559+shaikh44@users.noreply.github.com>
2020-04-22 19:11:20 +0530

    Update order_shop_list.php

--- buyer/request/order_shop_list.php
+++ buyer/request/order_shop_list.php
@@ -1,26 +1,36 @@
 <?php include('../../connect.php');
 
 $result_shops = $conn->query("SELECT DISTINCT shop_id FROM token_list WHERE buyer_id='$id'");
-while ($row_shops = $result_shops->fetch_assoc()) {
-    $shop_id = $row_shops['shop_id'];
+if ($result_shops->num_rows > 0) {
+    while ($row_shops = $result_shops->fetch_assoc()) {
 
-    $row_seller = $conn->query("SELECT * FROM sellers WHERE id='$shop_id'")->fetch_assoc();
+        $shop_id = $row_shops['shop_id'];
+
+        $row_seller = $conn->query("SELECT * FROM sellers WHERE id='$shop_id'")->fetch_assoc();
+        echo '' .
+            '<div class="card order-shop-card mb-3" data-shop-id=' . $shop_id . '>' .
+            '    <div class="card-body p-2 pr-3">' .
+            '        <div class="d-flex align-items-center justify-content-between">' .
+            '            <div class="d-flex align-items-center">' .
+            '               <div class="card-side-img"><img width="100%" height="100%" src="" alt=""></div>' .
+            '               <div class="ml-2">' .
+            '                   <p class="card-title h6 mb-1">' . $row_seller['name'] . '</p>' .
+            '                   <p class="card-sub-title small text-uppercase mb-0">' . $row_seller['category'] . '</p>' .
+            '               </div>' .
+            '            </div>' .
+            '            <div class="cart-display">' .
+            '                <div class="badge badge-warning">' . $count_token = $conn->query("SELECT DISTINCT token_number FROM `token_list` WHERE buyer_id='$id' AND shop_id='$shop_id'")->num_rows . '</div>' .
+            '                <button class="btn btn-primary view-tokens"><i class="fa fa-list-alt"></i></button>' .
+            '            </div>' .
+            '        </div>' .
+            '    </div>' .
+            '</div>';
+    }
+} else {
     echo '' .
-        '<div class="card order-shop-card mb-3" data-shop-id=' . $shop_id . '>' .
-        '    <div class="card-body p-2 pr-3">' .
-        '        <div class="d-flex align-items-center justify-content-between">' .
-        '            <div class="d-flex align-items-center">' .
-        '               <div class="card-side-img"><img width="100%" height="100%" src="" alt=""></div>' .
-        '               <div class="ml-2">' .
-        '                   <p class="card-title h6 mb-1">' . $row_seller['name'] . '</p>' .
-        '                   <p class="card-sub-title small text-uppercase mb-0">' . $row_seller['category'] . '</p>' .
-        '               </div>' .
-        '            </div>' .
-        '            <div class="cart-display">' .
-        '                <div class="badge badge-warning">' . $count_token = $conn->query("SELECT DISTINCT token_number FROM `token_list` WHERE buyer_id='$id' AND shop_id='$shop_id'")->num_rows . '</div>' .
-        '                <button class="btn btn-primary view-tokens"><i class="fa fa-list-alt"></i></button>' .
-        '            </div>' .
-        '        </div>' .
+        '<div class="card">' .
+        '    <div class="card-body text-center">' .
+        '        <p class="h3 font-weight-light mb-0">No Orders</p>' .
         '    </div>' .
         '</div>';
 } ?>
